ae6048e9af98610d202b71c8d36a261962dbfe6c
chuhx <hx.chu@clustar.ai>
2020-12-10 15:44:54 +0800

    Feat: searchPage done

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,48 +1,110 @@
 import React from 'react';
 import { Form, Input, Button, Descriptions } from 'antd';
+import { getSearch } from '@/services/home';
 import styles from './index.less';
 
-export default function Search() {
-  return (
-    <div className={styles.search_container}>
-      <Form layout="inline">
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: '请输入模型ID, 中间结果验证等' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            查询
-          </Button>
-        </Form.Item>
-      </Form>
-      <section className={styles.header}>
-        <Descriptions title="ModelID: datasets" column={2}>
-          <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
-          <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-          <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-          <Descriptions.Item label="Remark">empty</Descriptions.Item>
-          <Descriptions.Item label="Address">
-            No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-          </Descriptions.Item>
-        </Descriptions>
-      </section>
-      <section className={styles.content}>
-        <h4 className={styles.title}>
-          <Descriptions>
-            <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
-            <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-            <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-          </Descriptions>
-        </h4>
-        <Descriptions clsssName={styles.item} column={2}>
-          <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
-          <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-          <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-        </Descriptions>
-      </section>
-    </div>
-  );
+const headerModel = {
+  Algorithm: '模型算法',
+  StrongAudit: '强审计',
+  StartTime: '开始时间',
+  EndTime: '结束时间',
+};
+const contentHeaderModel = {
+  MSPID: '发起方',
+  OrgName: '组织者',
+  todo: '标示',
+};
+const contentItemModel = {
+  DataSetName: '数据集名称',
+  DataSetHash: '数据集MT Hash',
+  todo: '隐私求交发送',
+  PsiResultHash: '隐私交集  Hash',
+  todo0: '模型训练发送',
+  ModelHash: '模型结果  Hash',
+  Version: '客户端版本',
+  SignOff: '签字确认',
+};
+export default class Search extends React.Component {
+  state = {
+    searchData: {},
+  };
+
+  getSearch = async () => {
+    const data = await getSearch(1);
+    this.setState({
+      searchData: data,
+    });
+  };
+
+  onFinish = (val) => {
+    const { username } = val;
+    this.getSearch(username);
+  };
+
+  render() {
+    const { Participants } = this.state.searchData;
+    return (
+      <div className={styles.search_container}>
+        <Form layout="inline" onFinish={this.onFinish}>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: '请输入模型ID, 中间结果验证等' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              查询
+            </Button>
+          </Form.Item>
+        </Form>
+        {!Participants ? (
+          ''
+        ) : (
+          <>
+            <section className={styles.header}>
+              <Descriptions title="ModelID: datasets" column={2}>
+                {Object.keys(headerModel).map((item) => {
+                  return (
+                    <Descriptions.Item label={headerModel[item]}>
+                      {this.state.searchData[item] === true ? '是' : this.state.searchData[item]}
+                    </Descriptions.Item>
+                  );
+                })}
+              </Descriptions>
+            </section>
+            <section className={styles.content}>
+              {Participants &&
+                Participants.map((item) => {
+                  return (
+                    <>
+                      <h4 className={styles.title}>
+                        <Descriptions>
+                          {Object.keys(contentHeaderModel).map((subItem) => {
+                            return (
+                              <Descriptions.Item label={contentHeaderModel[subItem]}>
+                                {item[subItem]}
+                              </Descriptions.Item>
+                            );
+                          })}
+                        </Descriptions>
+                      </h4>
+                      <Descriptions clsssName={styles.item} column={2}>
+                        {Object.keys(contentItemModel).map((subItem) => {
+                          return (
+                            <Descriptions.Item label={contentItemModel[subItem]}>
+                              {item[subItem]}
+                            </Descriptions.Item>
+                          );
+                        })}
+                      </Descriptions>
+                    </>
+                  );
+                })}
+            </section>
+          </>
+        )}
+      </div>
+    );
+  }
 }
--- src/services/home.js
+++ src/services/home.js
@@ -1,7 +1,13 @@
 import request from '@/utils/request';
 
+// 获取数据概览
 export const getModel = () => {
   return request('abc/api/v1/datasets', {
     method: 'GET',
   });
+};
+
+// 获取链上查询
+export const getSearch = (id) => {
+  return request(`abc/api/v1/model/datasets?some_id=${id}`);
 };
